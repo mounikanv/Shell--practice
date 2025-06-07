0f25c659a6a20e30d1c9bb7b4683af17514a7195
mounika.nv <mounikanattama5b3.com>
2025-06-07 19:46:45 +0530

    Hello world created

---
+++ 02-conversastion.sh
@@ -0,0 +1,6 @@
+#! bin/bash
+echo "Tramp: hey musk, how are you?
+echo "Musk:: hello trumo,iam fine.how are you doing"
+echo "Trump: Iam fine too.whats up?
+echo "muak:: Nothing,just going to mars now, will you come"
+echo "Trump::sorry ,you carry om! i will come once you come back"
---
+++ 03-range-based for loop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for n in {1..5};
+do
+   echo$n
+   done
