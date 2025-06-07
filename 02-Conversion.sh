ec6c18dbd20eeb404455660cc6ec6396d6f830f2
mounika.nv <mounikanattama5b3.com>
2025-06-07 21:43:44 +0530

    Conversion

--- 02-Conversion.sh
+++ 02-Conversion.sh
@@ -1,6 +1,6 @@
-#! bin/bash
-echo "Tramp: hey musk, how are you?
-echo "Musk:: hello trumo,iam fine.how are you doing"
-echo "Trump: Iam fine too.whats up?
-echo "muak:: Nothing,just going to mars now, will you come"
-echo "Trump::sorry ,you carry om! i will come once you come back"
+#!bin/bash
+echo -e "Tramp: hey musk, how are you?
+echo -e "Musk:: hello trumo,iam fine.how are you doing"
+echo -e "Trump: Iam fine too.whats up?
+echo -e "muak:: Nothing,just going to mars now, will you come"
+echo -e "Trump::sorry ,you carry om! i will come once you come back"
