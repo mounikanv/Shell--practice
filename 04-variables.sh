df67504d736873e671242ee1ba8a1b9d46cebadb
mounika.nv <mounikanattama5b3.com>
2025-06-07 21:12:20 +0530

    variable

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+PERSON1=$1
+PERSON1=$2
+
+echo "$PERSON1: hey $PERSON2, how are you?
+echo "$PERSON2:: hello $PERSON1,iam fine.how are you doing"
+echo "$PERSON1: Iam fine too.whats up?
+echo "$PESON2:: Nothing,just going to mars now, will you come"
+echo "$PERSON1::sorry ,you carry om! i will come once you come back"
