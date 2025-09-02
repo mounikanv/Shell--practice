6e0eedc6c2e87e560f4573a632fb4934ae80a4d6
mounikanv <mounikanattama5b3@gmail.com>
2025-09-03 00:27:05 +0530

    hello world started

--- 12-colours.sh
+++ 12-colours.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo -e "\e [31m Hello colors \e[0m"
+echo -e "\e[31m Hello colors \e[0m"
 echo "Hello no colors"
-echo "-e "\e[32m Hello colors\e[0m"
-echo "-e "\e[33m Hello world"
 
 
